35d98bbdc63f2c60ae1867af4a936c63f7d499b5
Ирина Черняева <iridache@192.168.1.7>
2023-03-20 15:36:00 +0300

    slider added

---
+++ index.js
@@ -0,0 +1,17 @@
+let Swipes = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 6,
+        renderBullet: function (index, className) {
+            return `<div class=${className} style="background-image:url(./img/image-${index}.png)"></div>`;
+          },
+    },
+});
+    
