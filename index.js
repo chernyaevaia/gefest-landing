2fa5512cb0673eb2b3680244aa57cdce011a3e81
Ирина Черняева <iridache@192.168.1.7>
2023-03-20 16:24:18 +0300

    desktop width range responsive

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-let Swipes = new Swiper('.swiper', {
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+let Swipes = new Swiper(".swiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 6,
+    renderBullet: function (index, className) {
+      return `<div class=${className} style="background-image:url(./img/image-${index}.png)"></div>`;
     },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-        dynamicMainBullets: 6,
-        renderBullet: function (index, className) {
-            return `<div class=${className} style="background-image:url(./img/image-${index}.png)"></div>`;
-          },
-    },
+  },
 });
-    
