887e2eeb0af72f798f5a2df456b581f8fbd5b543
Ирина Черняева <iridache@192.168.1.7>
2023-03-21 14:04:16 +0300

    burger menu done

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ let Swipes = new Swiper(".swiper", {
     },
   },
 });
+
+let menuBtn = document.querySelector(".header__burgerMenu");
+let menuList = document.querySelector(".menu__list");
+let header = document.querySelector(".header");
+let menuLinks = document.querySelectorAll(".menu__link");
+let menuLines = document.querySelectorAll(".menu__line");
+let menu = document.querySelector(".menu");
+let apartment = document.querySelector(".apartment");
+let info = document.querySelector(".info");
+let overlay = document.querySelector(".overlay");
+let contactBtn = document.querySelector(".header__contactBtn");
+
+menuBtn.addEventListener("click", function () {
+  menuLines.forEach((e) => e.classList.toggle("active"));
+  menuLinks.forEach((e) => e.classList.toggle("active"));
+  menu.classList.toggle("active");
+  menuList.classList.toggle("active");
+  overlay.classList.toggle("active");
+  header.classList.toggle("active");
+  apartment.classList.toggle("active");
+  info.classList.toggle("active");
+  contactBtn.classList.toggle("active");
+});
